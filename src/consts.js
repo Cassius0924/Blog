f0489e5e8e73e3a956fcddd52b33ca74edb74043
Cassius0924 <2670226747@qq.com>
2023-03-24 12:31:26 +0800

    Docs: 添加头信息

--- src/consts.js
+++ src/consts.js
@@ -1,5 +1,5 @@
-export const SITE_TITLE = `Austin's Blog`;
-export const SITE_DESCRIPTION = 'Austin Site Description';
-export const SITE_EMAIL = 'no.sql@qq.com'
-export const SITE_NAME = 'astro.yufengbiji.com';
-export const SITE_URL = "https://astro.yufengbiji.com";
+export const SITE_TITLE = `Cassius0924's Blog`;
+export const SITE_DESCRIPTION = '分享我经历过的事情、学习过的知识、发现的好东西啦啦啦`';
+export const SITE_EMAIL = 'cassius0924@qq.com'
+export const SITE_NAME = 'www.theaurora.cn';
+export const SITE_URL = "http://www.theaurora.cn";
